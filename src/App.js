fbd2eadc6ce19a5b712260aed89b91ff19404ee1
Alena <mouse.programmouse@gmail.com>
2020-02-08 12:07:44 -0800

    Add a MainDescription component; add MainLogo;

--- src/App.js
+++ src/App.js
@@ -1,27 +1,37 @@
 import React, { Component } from 'react';
 import './App.css';
-//import components
+// import components
 import MainDescription from './components/MainDescription';
+import Logo from './imgs/main-logo.svg';
 import * as data from './dict.json';
 
 class App extends Component {
   constructor (props) {
     super(props);
     this.state = {
-      content: data.content,
+      content: data.content["RU"], // TODO: get lang from cookies;
       lang: "RU" // TODO: get lang from cookies;
     };
   }
 
   changeLang = (newLang) => {
-    this.setState({ lang: newLang });
+    this.setState({ 
+      content: data.content[newLang], // "translate" content
+      lang: newLang 
+    });
   }
 
   render () {
+    const content = this.state.content;
+  
     return (
       <div className="App">
         <header className="App-header">
-          <MainDescription header={this.state.content.RU.mainHeadline}/>
+          <MainDescription 
+            header = { content.mainHeadline }
+            description = { content.mainDescription }
+            logo = { Logo }
+            logoAlt = { "логотип ателье по ремонту одежды 'Татьяна'" } />
         </header>
       </div>
     );
--- src/components/MainDescription.js
+++ src/components/MainDescription.js
@@ -3,14 +3,20 @@ import PropTypes from 'prop-types';
 
 const MainDescription = (props) => {
 
-    const { header, description, logo } = props;
+    const { header, description, logo, logoAlt } = props;
 
     return (
         <div className="main-description">
-            <h1>{header}</h1>
-            <p>{description}</p>
+            <img src = { logo } alt = { logoAlt }/>
+            <h1>{ header }</h1>
+            <p>{ description }</p>
         </div>
     );
+}
+
+MainDescription.propTypes = {
+    header: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired
 }
 
 export default MainDescription
